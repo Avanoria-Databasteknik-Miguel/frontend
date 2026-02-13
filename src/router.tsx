4e67102fd7c491f1cd76923d9dc56792fc4a7485
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-13 19:20:03 +0100

    implemented routing for not found page.

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import HomePage from "./pages/HomePage";
+import CenterLayout from "./layouts/CenterLayout";
+import NotFoundPage from "./pages/NotFoundPage";
 
 
 export const router = createBrowserRouter([
@@ -11,6 +13,16 @@ export const router = createBrowserRouter([
                 element: <HomePage />,
                 path: "/",
                 handle: { title: "Home" },
+            }
+        ]
+    },
+    {
+        element: <CenterLayout />,
+        children: [
+            {
+                element: <NotFoundPage />,
+                path:"*",
+                handle: { title: "Not found" }
             }
         ]
     }
